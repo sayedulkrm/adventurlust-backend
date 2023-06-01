6028e6926726c9ff06a81aaadf4c7201d8b9a5cf
Sayedul Karim <100830568+sayedulkrm@users.noreply.github.com>
2023-06-01 20:12:01 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // Remember to add ".js" whille importing the file. Otherwise it will show error.
-connnectDB();
+
 import app from "./app.js";
 import { connnectDB } from "./config/database.js";
 
@@ -7,7 +7,7 @@ import cloudinary from "cloudinary";
 import Razorpay from "razorpay";
 import nodeCorn from "node-cron";
 import { Stats } from "./models/AdminStats.js";
-
+connnectDB();
 
 
 cloudinary.v2.config({
